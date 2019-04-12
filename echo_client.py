70edf64bb26d4cd3181bb1a8a9f275d2520d4a2c
motians <motians@comcast.net>
2019-04-11 20:09:09 -0700

    update and removed TODO sections, code working

--- echo_client.py
+++ echo_client.py
@@ -1,3 +1,7 @@
+"""
+Client socket
+"""
+
 import socket
 import sys
 import traceback
@@ -5,11 +9,10 @@ import traceback
 
 def client(msg, log_buffer=sys.stderr):
     server_address = ('127.0.0.1', 10000)
-    # TODO: Replace the following line with your code which will instantiate
-    #       a TCP socket with IPv4 Addressing, call the socket you make 'sock'
+
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
     print('connecting to {0} port {1}'.format(*server_address), file=log_buffer)
-    # TODO: connect your socket to the server here.
+
     sock.connect(server_address)
 
     # you can use this variable to accumulate the entire message received back
@@ -20,14 +23,9 @@ def client(msg, log_buffer=sys.stderr):
     # when we are finished with it
     try:
         print('sending "{0}"'.format(msg), file=log_buffer)
-        # TODO: send your message to the server here.
+
         sock.sendall(msg.encode('utf8'))
 
-        # TODO: the server should be sending you back your message as a series
-        #       of 16-byte chunks. Accumulate the chunks you get to build the
-        #       entire reply from the server. Make sure that you have received
-        #       the entire message and then you can break the loop.
-        #
         #       Log each chunk you receive.  Use the print statement below to
         #       do it. This will help in debugging problems
 
@@ -43,12 +41,10 @@ def client(msg, log_buffer=sys.stderr):
         traceback.print_exc()
         sys.exit(1)
     finally:
-        # TODO: after you break out of the loop receiving echoed chunks from
-        #       the server you will want to close your client socket.
+
         print('closing socket', file=log_buffer)
         sock.close()
 
-        # TODO: when all is said and done, you should return the entire reply
         # you received from the server as the return value of this function.
         return received_message
 
