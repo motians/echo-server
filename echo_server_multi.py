0bd7a40b85c173e7a84417255fcb971806e474eb
motians <motians@comcast.net>
2019-04-13 19:09:11 -0700

    finished multi connection echo server

--- echo_server_multi.py
+++ echo_server_multi.py
@@ -6,6 +6,7 @@ import socket
 import sys
 import traceback
 import select
+import queue
 
 
 def server(log_buffer=sys.stderr):
@@ -20,59 +21,62 @@ def server(log_buffer=sys.stderr):
     print("making a server on {0}:{1}".format(*address), file=log_buffer)
 
     sock.bind(address)
-    sock.listen(1)
+    sock.listen(5)
 
     inputs = [sock]
-    outputs = [sock]
+    outputs = []
+    message_queues = {}
 
     try:
         # the outer loop controls the creation of new connection sockets. The
         # server will handle each incoming connection one at a time.
-        while True:
-            print('waiting for a connection', file=log_buffer)
+        while inputs:
 
-            readserv, writeserv, excepserv = select.select(inputs, outputs, inputs)
+            read_server, write_server, excep_server = select.select(inputs, outputs, [])
 
-            conn, addr = sock.accept()
-
-            try:
-                print('connection - {0}:{1}'.format(*addr), file=log_buffer)
-
-                # the inner loop will receive messages sent by the client in
-                # buffers.  When a complete message has been received, the
-                # loop will exit
-                while True:
-
-                    data = conn.recv(16)
-                    print('received "{0}"'.format(data.decode('utf8')))
-
-                    # the fact using the print statement here.  It will help in
-                    # debugging problems.
-                    conn.sendall(data)
-                    print('sent "{0}"'.format(data.decode('utf8')))
-
-                    # of the message. If you have, then break from the `while True`
-                    # loop.
-                    # 
-                    # Figuring out whether or not you have received the end of the
-                    # message is a trick we learned in the lesson: if you don't
-                    # remember then ask your classmates or instructor for a clue.
-                    # :)
-                    if len(data) < 16:
+            for s in read_server:
+                if s is sock:
+                    conn, addr = s.accept()
+                    print('connection - {0}:{1}'.format(*addr), file=log_buffer)
+                    conn.setblocking(0)
+                    inputs.append(conn)
+                    message_queues[conn] = queue.Queue()
+                else:
+                    # Collecting 16 bytes of data at a time
+                    data = s.recv(16)
+                    if data:
+                        print('Received: {}'.format(data), file=log_buffer)
+                        message_queues[s].put(data)
+                        if s not in outputs:
+                            outputs.append(s)
                         break
+                    else:
+                        if s in outputs:
+                            outputs.remove(s)
+                        inputs.remove(s)
+                        print('Closing conn - {0}:{1}'.format(*addr), file=log_buffer)
+                        s.close()
+                        del message_queues[s]
 
-            except Exception as e:
-                traceback.print_exc()
-                sys.exit(1)
-            finally:
+            for s in write_server:
+                try:
+                    next_msg = message_queues[s].get_nowait()
+                    print('Sending: {}'.format(next_msg), file=log_buffer)
+                except queue.Empty:
+                    outputs.remove(s)
+                else:
+                    s.send(next_msg)
 
-                conn.close()
-                print(
-                    'echo complete, client connection closed', file=log_buffer
-                )
+            for s in excep_server:
+                inputs.remove(s)
+                if s in outputs:
+                    outputs.remove(s)
+                s.close()
+                del message_queues[s]
 
-    except KeyboardInterrupt:
-
+    except Exception as e:
+        traceback.print_exc()
+        print(f'Exception: {e}')
         sock.close()
         print('quitting echo server', file=log_buffer)
 
